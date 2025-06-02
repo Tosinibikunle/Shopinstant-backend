bcbe42452669cec90d405da48bb594bc08d19660
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-06-02 18:49:43 +0000

    settings mod3

--- settings.py
+++ settings.py
@@ -1,29 +1,4 @@
-                'django.contrib.sessions',
-                    'django.contrib.messages',
-                        'django.contrib.staticfiles',
-                            'rest_framework',
-                                'rest_framework_simplejwt',
-                                    'corsheaders',
-                                        'users',
-                                            'products',
-                                                'orders',
-                                                    'cart',
-                                                        'payments',
-                                                            'reviews',
-                                                                'coupons',
-                                                                    'core',
-                                                                    ]
 
-                                                                    MIDDLEWARE = [
-                                                                        'corsheaders.middleware.CorsMiddleware',
-                                                                            'django.middleware.security.SecurityMiddleware',
-                                                                                'django.contrib.sessions.middleware.SessionMiddleware',
-                                                                                    'django.middleware.common.CommonMiddleware',
-                                                                                        'django.middleware.csrf.CsrfViewMiddleware',
-                                                                                            'django.contrib.auth.middleware.AuthenticationMiddleware',
-                                                                                                'django.contrib.messages.middleware.MessageMiddleware',
-                                                                                                    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-                                                                                                    ]
 
                                                                                                     ROOT_URLCONF = 'backend.urls'
 
