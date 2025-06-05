53334bdd0ed226ec4676a83914c8f9c37fe1af98
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-06-05 19:18:11 +0000

    manage mod

--- manage.py
+++ manage.py
@@ -5,11 +5,10 @@ import os
 import sys
 
 def main():
-    """Run administrative tasks."""
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-            try:
-                    from django.core.management import execute_from_command_line
-                        except ImportError as exc:
+   os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+   try:
+      from django.core.management import execute_from_command_line
+      except ImportError as exc:
                                 raise ImportError(
                                             "Couldn't import Django. Are you sure it's installed and "
                                                         "available on your PYTHONPATH environment variable? Did you "
