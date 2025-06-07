349cf514f30ad075faf229b311d51f8d8e7f7af7
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-06-07 18:03:11 +0000

    mod1 admin

--- users/admin.py
+++ users/admin.py
@@ -6,13 +6,13 @@ from .models import User
 
 class UserAdmin(BaseUserAdmin):
     ordering = ['email']
-        list_display = ['email', 'first_name', 'last_name', 'is_vendor', 'is_staff']
-            fieldsets = (
-                    (None, {'fields': ('email', 'password')}),
-                            ('Personal Info', {'fields': ('first_name', 'last_name', 'phone_number')}),
-                                    ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_vendor', 'groups', 'user_permissions')}),
-                                            ('Important dates', {'fields': ('last_login', 'date_joined')}),
-                                                )
+    list_display = ['email', 'first_name', 'last_name', 'is_vendor', 'is_staff']
+    fieldsets = (
+       (None, {'fields': ('email', 'password')}),
+       ('Personal Info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+       ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_vendor', 'groups', 'user_permissions')}),
+       ('Important dates', {'fields': ('last_login', 'date_joined')}),
+   )
                                                     add_fieldsets = (
                                                             (None, {
                                                                         'classes': ('wide',),
