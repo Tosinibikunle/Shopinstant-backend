abc2a4bd7556b57817aee90abaff37e2c4bd7d31
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-06-10 12:14:32 +0000

    userurlmod

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import RegisterView, LoginView, ProfileView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-        path('login/', LoginView.as_view(), name='login'),
-            path('profile/', ProfileView.as_view(), name='profile'),
-            ]
+   path('register/', RegisterView.as_view(), name='register'),
+   path('login/', LoginView.as_view(), name='login'),
+   path('profile/', ProfileView.as_view(), name='profile'),
+   ]
