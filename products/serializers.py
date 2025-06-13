ef0b3593280e029d3db1ba25686a6ccb00b17820
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-06-13 16:39:57 +0000

    productserializer

---
+++ products/serializers.py
@@ -0,0 +1,24 @@
+# products/serializers.py
+
+from rest_framework import serializers
+from .models import Product, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+            model = Category
+                    fields = ['id', 'name', 'slug']
+
+                    class ProductSerializer(serializers.ModelSerializer):
+                        seller = serializers.StringRelatedField(read_only=True)
+                            category = CategorySerializer(read_only=True)
+                                category_id = serializers.PrimaryKeyRelatedField(
+                                        queryset=Category.objects.all(), source='category', write_only=True
+                                            )
+
+                                                class Meta:
+                                                        model = Product
+                                                                fields = [
+                                                                            'id', 'name', 'slug', 'description',
+                                                                                        'price', 'image', 'stock', 'is_active',
+                                                                                                    'category', 'category_id', 'seller', 'created_at'
+                                                                                                            ]
