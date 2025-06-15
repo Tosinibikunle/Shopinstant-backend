86cd8877d077dad39f7fc1077dff9fd11e80514a
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-06-15 15:56:09 +0000

    modprodurl

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
-# products/urls.py
+
 
 from django.urls import path
 from .views import CategoryListView, ProductListCreateView, ProductDetailView
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='category-list'),
-        path('products/', ProductListCreateView.as_view(), name='product-list-create'),
-            path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-            ]
+    path('products/', ProductListCreateView.as_view(), name='product-list-create'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+         ]
