cf6f9dcb77fcd3fb48b2fb3057a4812c0615f07e
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-06-16 18:22:33 +0000

    altersbackrl

--- urls.py
+++ urls.py
@@ -7,7 +7,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
-    path('api/products/', include('products.urls')),
-    path('api/orders/', include('orders.urls')),
-    path('api/cart/', include('cart.urls'
+    
