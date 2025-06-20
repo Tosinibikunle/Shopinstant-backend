fc9fc50b161a3bcf3c4a121d3980fb2bb000b489
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-06-20 13:43:17 +0000

    adminorder

--- orders/orderAdmin.py
+++ orders/orderAdmin.py
@@ -5,14 +5,15 @@ from .models import Order, OrderItem
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
-        extra = 0
+    extra = 0
 
-        @admin.register(Order)
-        class OrderAdmin(admin.ModelAdmin):
-            list_display = ('id', 'customer', 'created_at', 'is_paid', 'total_price')
-                list_filter = ('is_paid', 'created_at')
-                    search_fields = ('customer__email',)
-                        inlines = [OrderItemInline]
+    @admin.register(Order)
+
+class OrderAdmin(admin.ModelAdmin):
+     list_display = ('id', 'customer', 'created_at', 'is_paid', 'total_price')
+     list_filter = ('is_paid', 'created_at')
+     search_fields = ('customer__email',)
+     inlines = [OrderItemInline]
 
                         @admin.register(OrderItem)
                         class OrderItemAdmin(admin.ModelAdmin):
