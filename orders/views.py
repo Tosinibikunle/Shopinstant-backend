71e796222d26d67e0bf1770b114ee3df98ed3f38
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-06-22 21:11:55 +0000

    orderview

---
+++ orders/views.py
@@ -0,0 +1,19 @@
+# orders/views.py
+
+from rest_framework import generics, permissions
+from .models import Order
+from .serializers import OrderSerializer
+
+class OrderCreateView(generics.CreateAPIView):
+    serializer_class = OrderSerializer
+        permission_classes = [permissions.IsAuthenticated]
+
+            def perform_create(self, serializer):
+                    serializer.save(customer=self.request.user)
+
+                    class OrderListView(generics.ListAPIView):
+                        serializer_class = OrderSerializer
+                            permission_classes = [permissions.IsAuthenticated]
+
+                                def get_queryset(self):
+                                        return Order.objects.filter(customer=self.request.user)
