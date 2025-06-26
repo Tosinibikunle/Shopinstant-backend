b9df4e1cf5e0e7d0b497cc167f0cfc6e3c85ed40
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-06-26 17:16:06 +0000

    cartadmin

---
+++ cart/admin.py
@@ -0,0 +1,10 @@
+# cart/admin.py
+
+from django.contrib import admin
+from .models import CartItem
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product', 'quantity', 'added_at')
+        search_fields = ('user__email', 'product__name')
+            list_filter = ('added_at',)
