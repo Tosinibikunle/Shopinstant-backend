8687d16e9dc2db2b097c1c8a26010a1a2c66ac1e
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-06-28 19:33:51 +0000

    cartviews

---
+++ cart/views.py
@@ -0,0 +1,31 @@
+# cart/views.py
+
+from rest_framework import generics, permissions
+from .models import CartItem
+from .serializers import CartItemSerializer
+
+class CartItemListCreateView(generics.ListCreateAPIView):
+    serializer_class = CartItemSerializer
+        permission_classes = [permissions.IsAuthenticated]
+
+            def get_queryset(self):
+                    return CartItem.objects.filter(user=self.request.user)
+
+                        def perform_create(self, serializer):
+                                existing_item = CartItem.objects.filter(
+                                            user=self.request.user,
+                                                        product=serializer.validated_data['product']
+                                                                ).first()
+
+                                                                        if existing_item:
+                                                                                    existing_item.quantity += serializer.validated_data.get('quantity', 1)
+                                                                                                existing_item.save()
+                                                                                                        else:
+                                                                                                                    serializer.save(user=self.request.user)
+
+                                                                                                                    class CartItemUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+                                                                                                                        serializer_class = CartItemSerializer
+                                                                                                                            permission_classes = [permissions.IsAuthenticated]
+
+                                                                                                                                def get_queryset(self):
+                                                                                                                                        return CartItem.objects.filter(user=self.request.user)
