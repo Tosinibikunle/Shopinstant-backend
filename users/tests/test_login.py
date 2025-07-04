b257d4add6b2cddba17b3cb3f11037b7a3b7c0e2
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-07-04 22:03:49 +0000

    new testuser login

--- users/tests/test_login.py
+++ users/tests/test_login.py
@@ -10,21 +10,21 @@ User = get_user_model()
 class LoginUserTest(APITestCase):
    def setUp(self):
       self.user = User.objects.create_user(
-                        email="login@example.com",
-                                    first_name="Login",
-                                                last_name="Tester",
-                                                            phone_number="08099998888",
-                                                                        password="testpass123"
-                                                                                )
+      email="login@example.com",
+      first_name="Login",
+      last_name="Tester",
+      phone_number="08099998888",
+      password="testpass123"
+        )
 
-                                                                                    def test_user_can_login_with_valid_credentials(self):
-                                                                                            url = reverse('login')
-                                                                                                    data = {
-                                                                                                                "email": "login@example.com",
-                                                                                                                            "password": "testpass123"
-                                                                                                                                    }
-                                                                                                                                            response = self.client.post(url, data)
-                                                                                                                                                    self.assertEqual(response.status_code, status.HTTP_200_OK)
-                                                                                                                                                            self.assertIn("token", response.data)
-                                                                                                                                                                    self.assertIn("access", response.data["token"])
-                                                                                                                                                                            self.assertIn("refresh", response.data["token"])
+   def test_user_can_login_with_valid_credentials(self):
+      url = reverse('login')
+      data = {
+         "email": "login@example.com",
+         "password": "testpass123"
+                     }
+      response = self.client.post(url, data)
+      self.assertEqual(response.status_code, status.HTTP_200_OK)
+      self.assertIn("token", response.data)
+      self.assertIn("access", response.data["token"])
+      self.assertIn("refresh", response.data["token"])
