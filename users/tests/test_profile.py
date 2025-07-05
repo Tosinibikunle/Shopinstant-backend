e97d0a51b5899fb943c672ffaed02751636e4a4d
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-07-05 21:00:04 +0000

    test profile user

---
+++ users/tests/test_profile.py
@@ -0,0 +1,28 @@
+# users/tests/test_profile.py
+
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+
+class UserProfileTest(APITestCase):
+    def setUp(self):
+            self.user = User.objects.create_user(
+                        email="profile@example.com",
+                                    first_name="Profile",
+                                                last_name="Tester",
+                                                            phone_number="08123456789",
+                                                                        password="pass4567"
+                                                                                )
+                                                                                        refresh = RefreshToken.for_user(self.user)
+                                                                                                self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {refresh.access_token}")
+
+                                                                                                    def test_authenticated_user_can_view_profile(self):
+                                                                                                            url = reverse('profile')
+                                                                                                                    response = self.client.get(url)
+                                                                                                                            self.assertEqual(response.status_code, status.HTTP_200_OK)
+                                                                                                                                    self.assertEqual(response.data['email'], self.user.email)
+                                                                                                                                            self.assertEqual(response.data['first_name'], self.user.first_name)
