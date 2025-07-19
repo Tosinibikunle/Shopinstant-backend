db82eabdb4adc1010cd6d4e9b59af6ac3e4521af
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-07-19 19:52:37 +0000

    serialshipping mod

--- shippings/serializers.py
+++ shippings/serializers.py
@@ -4,18 +4,15 @@ from .models import ShippingAddress, ShippingMethod
 
 class ShippingAddressSerializer(serializers.ModelSerializer):
     class Meta:
-            model = ShippingAddress
-                    fields = [
-                                'id', 'full_name', 'address_line1', 'address_line2', 
-                                            'city', 'state', 'postal_code', 'country', 
-                                                        'phone_number', 'is_default', 'created_at'
-                                                                ]
-                                                                        read_only_fields = ['id', 'created_at']
+        model = ShippingAddress
+        fields = [
+           'id', 'full_name', 'address_line1', 'address_line2', 'city', 'state', 'postal_code', 'country','phone_number', 'is_default', 'created_at'
+                  ]
+        read_only_fields = ['id', 'created_at']
 
-                                                                            def create(self, validated_data):
-                                                                                    # Automatically associate the shipping address with the logged-in user
-                                                                                            validated_data['user'] = self.context['request'].user
-                                                                                                    return super().create(validated_data)
+        def create(self, validated_data):
+           validated_data['user'] = self.context['request'].user
+           return super().create(validated_data)
 
                                                                                                     class ShippingMethodSerializer(serializers.ModelSerializer):
                                                                                                         class Meta:
