cdc8d4ef9897bb7da568a6e7fbb4c90c7e664266
Tosinibikunle <tosinibikunle1@gmail.com>
2025-07-28 21:18:43 +0100

    shippings tests mod

--- shippings/tests.py
+++ shippings/tests.py
@@ -1,24 +1,27 @@
 from django.test import TestCase
 from .models import ShippingMethod
 
+
 class ShippingMethodTestCase(TestCase):
     def setUp(self):
-            # Set up initial test data
-                    ShippingMethod.objects.create(name="Standard", cost=10.00, estimated_days=5)
-                            ShippingMethod.objects.create(name="Express", cost=25.00, estimated_days=2)
+        # Set up initial test data
+        ShippingMethod.objects.create(
+            name="Standard", cost=10.00, estimated_days=5)
+        ShippingMethod.objects.create(
+            name="Express", cost=25.00, estimated_days=2)
 
-                                def test_shipping_method_count(self):
-                                        """Ensure two shipping methods exist."""
-                                                self.assertEqual(ShippingMethod.objects.count(), 2)
+    def test_shipping_method_count(self):
+        """Ensure two shipping methods exist."""
+        self.assertEqual(ShippingMethod.objects.count(), 2)
 
-                                                    def test_shipping_method_names(self):
-                                                            """Verify the names of shipping methods."""
-                                                                    standard = ShippingMethod.objects.get(name="Standard")
-                                                                            express = ShippingMethod.objects.get(name="Express")
-                                                                                    self.assertEqual(str(standard), "Standard")
-                                                                                            self.assertEqual(str(express), "Express")
+    def test_shipping_method_names(self):
+         """Verify the names of shipping methods."""
+         standard = ShippingMethod.objects.get(name="Standard")
+         express = ShippingMethod.objects.get(name="Express")
+         self.assertEqual(str(standard), "Standard")
+         self.assertEqual(str(express), "Express")
 
-                                                                                                def test_shipping_cost(self):
-                                                                                                        """Check that the cost is stored correctly."""
-                                                                                                                express = ShippingMethod.objects.get(name="Express")
-                                                                                                                        self.assertEqual(express.cost, 25.00)
+    def test_shipping_cost(self):
+        """Check that the cost is stored correctly."""
+        express = ShippingMethod.objects.get(name="Express")
+        self.assertEqual(express.cost, 25.00)
