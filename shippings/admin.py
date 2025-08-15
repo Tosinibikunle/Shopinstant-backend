849f2fd1351a2ae1e02f0f40e1a487d010926050
Tosinibikunle <tosinibikunle1@gmail.com>
2025-08-15 22:14:49 +0100

    format v4 and shippings admin

--- shippings/admin.py
+++ shippings/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import ShippingAddress, ShippingMethod, ShippingRate  
+
 # Register your models here.
+@admin.register(ShippingAddress)
+class ShippingAddressAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "full_name",
+        "city",
+        "state",
+        "postal_code",
+        "country",
+        "is_default",
+        "created_at",
+    )
+    search_fields = ("user__email", "full_name", "city", "state", "postal_code")
+    list_filter = ("is_default", "country", "created_at")
--- shippings/apps.py
+++ shippings/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ShippingsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shippings'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "shippings"
