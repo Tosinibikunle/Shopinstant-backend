920c68f03d31971eb1b0956fcd8373f81e1a441f
Tosinibikunle <tosinibikunle1@gmail.com>
2025-08-22 18:47:55 +0100

    shippings views

--- shippings/serializers.py
+++ shippings/serializers.py
@@ -22,7 +22,7 @@ class ShippingAddressSerializer(serializers.ModelSerializer):
         read_only_fields = ["id", "created_at"]
 
     def create(self, validated_data):
-      
+
         validated_data["user"] = self.context["request"].user
         return super().create(validated_data)
 
--- shippings/views.py
+++ shippings/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+# Create your views here
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated
+from .models import ShippingAddress, ShippingMethod
+from .serializers import ShippingAddressSerializer, ShippingMethodSerializer
+
+class ShippingAddressViewSet(viewsets.ModelViewSet):
+    serializer_class = ShippingAddressSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Only return addresses of the logged-in user
+        return ShippingAddress.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class ShippingMethodViewSet(viewsets.ModelViewSet):
+    queryset = ShippingMethod.objects.all()
+    serializer_class = ShippingMethodSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
