6188dff86f9175e75f27e845bfdc22e9c86575f9
Tosinibikunle <tosinibikunle1@gmail.com>
2025-08-25 17:55:29 +0100

    views and serializers for customer care app

--- customer_care/serializers.py
+++ customer_care/serializers.py
@@ -18,3 +18,34 @@ class TicketSerializer(serializers.ModelSerializer):
             "is_resolved",
         ]
         read_only_fields = ["id", "user", "created_at", "is_resolved"]
+
+
+class ResponseSerializer(serializers.ModelSerializer):
+    customer_care_rep = serializers.StringRelatedField(read_only=True)
+    ticket = serializers.PrimaryKeyRelatedField(queryset=Ticket.objects.all())
+
+    class Meta:
+        model = Response
+        fields = [
+            "id",
+            "customer_care_rep",
+            "ticket",
+            "message",
+            "created_at",
+        ]
+        read_only_fields = ["id", "customer_care_rep", "created_at"]        
+class FeedbackSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    ticket = serializers.PrimaryKeyRelatedField(queryset=Ticket.objects.all(), required=False, allow_null=True)
+
+    class Meta:
+        model = Feedback
+        fields = [
+            "id",
+            "ticket",
+            "user",
+            "message",
+            "rating",
+            "created_at",
+        ]
+        read_only_fields = ["id", "user", "created_at"]
--- customer_care/views.py
+++ customer_care/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, permissions
+from .models import Ticket, Response, Feedback
+from .serializers import TicketSerializer, ResponseSerializer, FeedbackSerializer
 # Create your views here.
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class ResponseViewSet(viewsets.ModelViewSet):
+    queryset = Response.objects.all()
+    serializer_class = ResponseSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(customer_care_rep=self.request.user)
+class FeedbackViewSet(viewsets.ModelViewSet):
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
