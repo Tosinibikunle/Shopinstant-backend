9a30108fecbe29785cc91b0222c5a6545d0ee8ae
TosinIbikunle <tosinibikunle1@gmail.com>
2025-10-19 21:59:33 +0100

    updated settings and urls to include home app

--- backend/urls.py
+++ backend/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/products/', include('products.urls')),
