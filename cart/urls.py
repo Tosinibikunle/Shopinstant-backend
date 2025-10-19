e038da41a946814053bb81cc6fda281c699a5591
TosinIbikunle <tosinibikunle1@gmail.com>
2025-10-19 22:17:36 +0100

    adding home page route to backend/urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,11 @@
 
 
 from django.urls import path
+from django.shortcuts import render
 from .views import CartItemListCreateView, CartItemUpdateDeleteView
 
 urlpatterns = [
     path('', CartItemListCreateView.as_view(), name='cart-list-create'),
     path('<int:pk>/', CartItemUpdateDeleteView.as_view(), name='cart-update-delete'),
+    path('', lambda request: render(request, './templates/home.html')),  # Updated line to render home.html
         ]
