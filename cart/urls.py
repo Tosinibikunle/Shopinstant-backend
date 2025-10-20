e634322ab9550f184154cd30291832255b2b5af5
TosinIbikunle <tosinibikunle1@gmail.com>
2025-10-20 22:06:58 +0100

    implementing a homepage route

--- cart/urls.py
+++ cart/urls.py
@@ -7,5 +7,4 @@ from .views import CartItemListCreateView, CartItemUpdateDeleteView
 urlpatterns = [
     path('', CartItemListCreateView.as_view(), name='cart-list-create'),
     path('<int:pk>/', CartItemUpdateDeleteView.as_view(), name='cart-update-delete'),
-    path('', lambda request: render(request, './templates/home.html')),  # Updated line to render home.html
         ]
