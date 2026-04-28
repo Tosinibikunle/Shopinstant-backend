b08f7f37fb71712ebae3a06739932f37084f806e
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-04-28 21:05:29 +0100

    Refactor orderAdmin.py by removing comments
    Removed commented-out code and cleaned up formatting.

--- orders/orderAdmin.py
+++ orders/orderAdmin.py
@@ -1,5 +1,3 @@
-# orders/admin.py
-
 from django.contrib import admin
 from .models import Order, OrderItem
 
@@ -8,14 +6,12 @@ class OrderItemInline(admin.TabularInline):
     model = OrderItem
     extra = 0
 
-
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("id", "customer", "created_at", "is_paid", "total_price")
     list_filter = ("is_paid", "created_at")
     search_fields = ("customer__email",)
     inlines = [OrderItemInline]
-
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
