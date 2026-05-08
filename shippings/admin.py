2112f8bfe38ea639c1f4eb540ff61455b7d5a7f5
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-05-08 21:54:51 +0100

    Update list_filter in ShippingAddressAdmin
    Removed 'country' from the list_filter in ShippingAddressAdmin.

--- shippings/admin.py
+++ shippings/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import ShippingAddress, ShippingMethod
 
 
-# Register your models here.
+
 @admin.register(ShippingAddress)
 class ShippingAddressAdmin(admin.ModelAdmin):
     list_display = (
@@ -17,4 +17,4 @@ class ShippingAddressAdmin(admin.ModelAdmin):
         "created_at",
     )
     search_fields = ("user__email", "full_name", "city", "state", "postal_code")
-    list_filter = ("is_default", "country", "created_at")
+    list_filter = ("is_default", "created_at")
