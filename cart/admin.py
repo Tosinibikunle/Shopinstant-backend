1896cc5212de126d6b24f5e819a4720c5b386d2a
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-06-13 20:18:19 +0100

    Update CartItemAdmin fields for display and search

--- cart/admin.py
+++ cart/admin.py
@@ -5,6 +5,6 @@ from .models import CartItem
 
 @admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
-    list_display = ('user', 'product', 'quantity', 'added_at')
-    search_fields = ('user__email', 'product__name')
+    list_display = ('user','user_id', 'product', 'quantity', 'added_at')
+    search_fields = ('user__email', 'product__name', 'product_id')
     list_filter = ('added_at',)
